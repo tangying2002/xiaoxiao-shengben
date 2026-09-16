618e4e4efaf7bc3209e5322ff17b73d28b90a6bf
tang2002 <156333345+tangying2002@users.noreply.github.com>
2026-09-16 19:50:22 +0800

    Update mobile app interface: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='xiaoxiao-shengben-v1';
+const CACHE='xiaoxiao-shengben-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./pwa-mobile.js','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
